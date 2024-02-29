6c73580a83547081bd77f608bc0fe8b15a54d9f3
Gihong Yim <gyim@c2r8s1.42seoul.kr>
2024-02-29 20:50:01 +0900

    ex00 Graph Vector2 added

---
+++ Module_00/ex01/Graph.hpp
@@ -0,0 +1,12 @@
+#ifndef __GRAPH_HPP__
+# define __GRAPH_HPP__
+
+class Graph {
+private:
+    int size;
+    Vector2 v;
+
+public:
+
+};
+#endif
---
+++ Module_00/ex01/Vector2.hpp
@@ -0,0 +1,8 @@
+#ifndef __VECTOR2_HPP__
+# define __VECTOR2_HPP__
+
+class Vector2 {
+private:
+    float x, y;
+};
+#endif
