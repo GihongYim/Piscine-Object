c7e6bdbb10bad0cedc4bf476bf9e18db042ce4c1
Gihong Yim <gyim@c2r8s1.42seoul.kr>
2024-04-24 21:04:35 +0900

    asdf

--- Module_01/ex00/Shovel.hpp
+++ Module_01/ex00/Shovel.hpp
@@ -2,6 +2,9 @@
 #include "Tool.hpp"
 #include "Worker.hpp"
 
+// Aggregation : Worker과 소유(Aggregation)관계이다.
+// Worker가 전체 잭체 Shovel이 부분 객체인 소유 관계에 있다.
+// Worker가 사라진다고 해서 Shovel이 사라지지는 않는것처럼 클래스 생명주기가 독립적이다.
 
 class Shovel : public Tool {
 
--- Module_01/ex00/Worker.hpp
+++ Module_01/ex00/Worker.hpp
@@ -5,6 +5,10 @@
 #include "Position.hpp"
 #include "Statistic.hpp"
 #include "Tool.hpp"
+// Worker과 Shovel(Tool)의 관계는 Aggregation 관계이므로 클래스의 생명 주기가 독립적이다.
+// 따라서 Worker가 사라지더라도 Shovel(Tool)은 사라지지 않아야한다.
+// 그러한 이유로 Worker는 Shovel(Tool)을 pointer값만 저장하였다.
+
 class Worker {
 
 private:
