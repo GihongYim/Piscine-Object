74d7ade289daef516d9dd130cc582ba7af7b80e6
Gihong Yim <gyim@c2r8s1.42seoul.kr>
2024-05-01 21:22:53 +0900

    asdf

--- Module_03/ex02/triangle.hpp
+++ Module_03/ex02/triangle.hpp
@@ -1,15 +1,16 @@
 #ifndef __TRIANGLE_HPP__
 #define __TRIANGLE_HPP__
 
+#include <cmath>
 #include "shape.hpp"
 
 class Triangle : public Shape
 {
 private:
-    double _side1, _side2, _side3, _base, _height;
+    double _side1, _side2, _side3;
 public:
-    Triangle(double side1, double side2, double side3, double base, double height) {
-        if (side1 < 0.0 || side2 < 0.0 || side3 < 0.0 || base < 0.0 || height < 0.0) {
+    Triangle(double side1, double side2, double side3) {
+        if (side1 < 0.0 || side2 < 0.0 || side3 < 0.0) {
             throw "number should be positive number";
         }
         if (side1 > side2 || side2 > side3 || side1 > side3) {
@@ -21,11 +22,13 @@ public:
         _side1 = side1;
         _side2 = side2;
         _side3 = side3;
-        _base = base;
-        _height = height;
     }
     virtual double calculateArea() const {
-        return 0.5 * _base * _height;
+        double s = (_side1 + _side2 + _side3) / 2.0;
+        double areaSquared = s * (s - _side1) * (s - _side2) * (s - _side3);
+        double area = sqrt(areaSquared);
+    
+        return area;
     }
 
     virtual double calculatePerimeter() const {
--- Module_03/ex03/employeeManagement.hpp
+++ Module_03/ex03/employeeManagement.hpp
@@ -1,26 +1,34 @@
 #ifndef __EMPLOYEEMANAGEMENT_HPP__
 #define __EMPLOYEEMANAGEMENT_HPP__
 
+#include <vector>
 #include "employee.hpp"
 
 class EmployeeManager 
 {
 private:
-     
+    std::vector<Employee*> employeed;
 public:
     void addEmployee(Employee* employee) 
     {
-
+        employeed.push_back(employee);
     }
 
     void removeEmployee(Employee* employee)
     {
-
+        for (int i = 0; i < employeed.size(); i++) {
+            if (employee == employeed[i]) {
+                employeed.erase(employeed.begin() + i);
+                break;
+            }
+        }
     }
 
     void excuteWorkday()
     {
-
+        for (int i = 0; i < employeed.size(); i++) {
+            employeed[i]->excuteWorkday();
+        }
     }
 
     void calculatePayroll()
