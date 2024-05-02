3927993ae3c9f6a6948c6011fee4adc3227f3305
Gihong Yim <gyim@c2r8s1.42seoul.kr>
2024-05-02 16:05:02 +0900

    module03 ex03

--- Module_03/ex03/employee.hpp
+++ Module_03/ex03/employee.hpp
@@ -1,6 +1,7 @@
 #ifndef __EMPLOYEE_HPP__
 #define __EMPLOYEE_HPP__
 
+#include <vector>
 #include "hourlyEmployee.hpp"
 #include "salariedEmployee.hpp"
 class Employee
@@ -8,24 +9,42 @@ class Employee
 protected:
     int hourlyValue;
 public:
-    int excuteWorkday(){
-
-    }
+    int excuteWorkday(){ }
 };
 
 class TempWorker: protected Employee, IHourlyEmployee
 {
-
+private:
+    std::vector<int> workingHour;
+public:
+    void registerWorkingHour(int hour)  {
+        workingHour.push_back(hour);
+    }   
 };
 
 class ContractEmployee: protected Employee, ISalariedEmployee
 {
-
+private:
+    std::vector<std::string> notWorking;
+public:
+    void registerNonWorkingHour(std::string reason) {
+        notWorking.push_back(reason);
+    }
 };
 
 class Apprentice: protected Employee, ISalariedEmployee
 {
+private:
+    std::vector<int> schoolHour;
+    std::vector<std::string> notWorking;
+public:
+    void registerNonWorkingHour(std::string reason) {
+        notWorking.push_back(reason);
+    }
 
+    void schoolHour(int hour) {
+        schoolHour.push_back(hour);
+    }
 };
 
 #endif
